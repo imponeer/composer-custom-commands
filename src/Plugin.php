c15b56623dc4167cf346679eab65ca40c3e855fb
Raimondas Rimkevičius <github@mekdrop.name>
2018-03-20 01:02:47 +0200

    PHP 5.5 and capable interface

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,8 @@ namespace Imponeer\ComposerCustomCommands;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Plugin\Capability\CommandProvider;
+use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 use Imponeer\ComposerCustomCommands\Exceptions\CommandsConfigIsNotArrayException;
 
@@ -12,49 +14,51 @@ use Imponeer\ComposerCustomCommands\Exceptions\CommandsConfigIsNotArrayException
  *
  * @package Imponeer\ComposerCustomCommand
  */
-class Plugin implements PluginInterface
+class Plugin implements PluginInterface, Capable
 {
-    /**
-     * Array with all commands classes that should be registered
-     *
-     * @var array
-     */
-    private static $commands_classes = array();
+	/**
+	 * Array with all commands classes that should be registered
+	 *
+	 * @var array
+	 */
+	private static $commands_classes = array();
 
-    /**
-     * Gets commands classes for reading outside class
-     *
-     * @return array
-     */
-    public static function getCommandsClasses() {
-        return self::$commands_classes;
-    }
+	/**
+	 * Gets commands classes for reading outside class
+	 *
+	 * @return array
+	 */
+	public static function getCommandsClasses()
+	{
+		return self::$commands_classes;
+	}
 
-    /**
-     * Method executed when activating plugin
-     *
-     * @param Composer $composer    Composer instance
-     * @param IOInterface $io       IO interface
-     */
-    public function activate(Composer $composer, IOInterface $io)
-    {
-        if ($composer->getConfig()->has('commands')) {
-            self::$commands_classes = $composer->getConfig()->get('commands');
-            if (!is_array(self::$commands_classes)) {
-                throw new CommandsConfigIsNotArrayException();
-            }
-        }
-    }
+	/**
+	 * Method executed when activating plugin
+	 *
+	 * @param Composer $composer Composer instance
+	 * @param IOInterface $io IO interface
+	 */
+	public function activate(Composer $composer, IOInterface $io)
+	{
+		$extra = $composer->getPackage()->getExtra();
+		if (isset($extra['commands'])) {
+			if (!is_array($extra['commands'])) {
+				throw new CommandsConfigIsNotArrayException();
+			}
+			self::$commands_classes = $extra['commands'];
+		}
+	}
 
-    /**
-     * Gets capabilities of object
-     *
-     * @return array
-     */
-    public function getCapabilities()
-    {
-        return array(
-            'Composer\Plugin\Capability\CommandProvider' => 'Imponeer\ComposerCustomCommand\CommandProvider',
-        );
-    }
+	/**
+	 * Gets capabilities of object
+	 *
+	 * @return array
+	 */
+	public function getCapabilities()
+	{
+		return array(
+			CommandProvider::class => \Imponeer\ComposerCustomCommands\CommandProvider::class
+		);
+	}
 }
